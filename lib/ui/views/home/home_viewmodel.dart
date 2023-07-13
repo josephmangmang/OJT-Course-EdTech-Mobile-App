219126a3a1f7ab6e6a6fecfc7874453c818e7225
josephmangmang <joseph@blendit.eu>
2023-07-13 12:46:32 +0800

    Remove unused codes

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,10 +1,7 @@
-import 'package:edtechapp/app/app.bottomsheets.dart';
-import 'package:edtechapp/app/app.dialogs.dart';
 import 'package:edtechapp/app/app.locator.dart';
 import 'package:edtechapp/model/user.dart';
 import 'package:edtechapp/services/shared_service.dart';
 import 'package:edtechapp/ui/common/app_constants.dart';
-import 'package:edtechapp/ui/common/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
