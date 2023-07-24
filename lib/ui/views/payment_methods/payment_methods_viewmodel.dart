0ca6a4cd297e7222b42afec70289f4cd9f0a7864
Arsenio Pacatang Jr <126472547+ArsenioPacatangJr@users.noreply.github.com>
2023-07-24 15:32:19 +0800

    implement payment

--- lib/ui/views/payment_methods/payment_methods_viewmodel.dart
+++ lib/ui/views/payment_methods/payment_methods_viewmodel.dart
@@ -3,13 +3,28 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/app.locator.dart';
+import '../../../model/credit_card.dart';
+import '../../../services/repository_service.dart';
 import '../add_credit_card/add_credit_card_view.dart';
 
 class PaymentMethodsViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
   final _snackBarService = locator<SnackbarService>();
+  final _repository= locator<RepositoryService>();
   bool isMasterCardClicked = false;
   bool isVisaCardClicked = false;
+  bool existPaymentMethod = false;
+  CreditCard? creditCard;
+
+  init() async {
+    setBusy(true);
+    await _repository.getCreditCard().then((value) {
+      if (value.isNotEmpty) {
+        existPaymentMethod = false;
+      }
+    });
+    setBusy(false);
+  }
 
   void onMasterCardClick() {
     isMasterCardClicked = true;
