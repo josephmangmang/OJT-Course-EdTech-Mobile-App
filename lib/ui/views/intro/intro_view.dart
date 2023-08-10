d112faebc5f7430617e8ab11d857f3d5f474640f
warrenbellingan <126529527+warrenbellingan@users.noreply.github.com>
2023-08-10 15:30:13 -0700

    fixing intro view bugs

--- lib/ui/views/intro/intro_view.dart
+++ lib/ui/views/intro/intro_view.dart
@@ -1,3 +1,4 @@
+import 'package:edtechapp/app/app.dart';
 import 'package:edtechapp/resources/png_images.dart';
 import 'package:edtechapp/ui/common/app_constants.dart';
 import 'package:edtechapp/ui/custom_widget/app_button.dart';
@@ -8,287 +9,83 @@ import 'intro_viewmodel.dart';
 class IntroPage extends StackedView<IntroViewModel> {
   IntroPage({Key? key}) : super(key: key);
 
-  final PageController pageController = PageController(initialPage: 0);
-
   @override
   Widget builder(
     BuildContext context,
     IntroViewModel viewModel,
     Widget? child,
   ) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        width: 375,
-        height: 812,
-        clipBehavior: Clip.antiAlias,
-        decoration: const BoxDecoration(color: Colors.white),
-        child: Stack(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
           children: [
-            PageView(
-              controller: pageController,
-              physics: const AlwaysScrollableScrollPhysics(),
-              onPageChanged: (int index) {
-                if (index == 2) {
-                  viewModel.changeText();
-                }
-                viewModel.updateCurrentPageIndex(index);
-
-                if (index < 2) {
-                  viewModel.changeTextToNext();
-                }
-                viewModel.updateCurrentPageIndex(index);
-              },
-              children: [
-                Container(
-                  width: 375,
-                  height: 812,
-                  decoration: const BoxDecoration(color: Colors.white),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 188,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 375,
-                              height: 264,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(PngImages
-                                      .coolKidsLongDistanceRelationship1),
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            const SizedBox(
-                              width: 375,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    AppConstants.intro1TitleText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF3B3936),
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.33,
-                                      letterSpacing: -0.50,
-                                    ),
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    AppConstants.introDescriptionText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF78746D),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                      height: 1.5,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 375,
-                  height: 812,
-                  decoration: const BoxDecoration(color: Colors.white),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 188,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 375,
-                              height: 264,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image:
-                                      AssetImage(PngImages.coolKidsStayingHome),
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            const SizedBox(
-                              width: 375,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    AppConstants.intro2TitleText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF3B3936),
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.33,
-                                      letterSpacing: -0.50,
-                                    ),
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    AppConstants.introDescriptionText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF78746D),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                      height: 1.5,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 375,
-                  height: 812,
-                  decoration: const BoxDecoration(color: Colors.white),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 188,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 375,
-                              height: 264,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image:
-                                      AssetImage(PngImages.coolKidsHighTech1),
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            const SizedBox(
-                              width: 375,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    AppConstants.intro3TitleText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF3B3936),
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.33,
-                                      letterSpacing: -0.50,
-                                    ),
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    AppConstants.introDescriptionText,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Color(0xFF78746D),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                      height: 1.5,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Positioned(
-              right: 10,
-              top: 60,
-              child: ElevatedButton(
-                onPressed: () {
-                  viewModel.goToLoginPage();
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(16),
-                  backgroundColor: Colors.transparent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  elevation: 0,
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                ),
-                child: const Text(
-                  AppConstants.skipText,
-                  style: TextStyle(
-                    color: Color(0xFF78746D),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    height: 1.14,
+            Container(
+              alignment: Alignment.centerRight,
+              child: InkWell(
+                onTap: viewModel.goToLoginPage,
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    AppConstants.skipText,
+                    style: TextStyle(
+                      color: Color(0xFF78746D),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ),
             ),
-            Positioned(
-                left: 50,
-                right: 30,
-                top: 695,
-                bottom: 25,
-                child: AppButton(
-                  title: viewModel.buttonText,
-                  onClick: () {
-                    // ignore: non_constant_identifier_names
-                    final int currentPageIndex =
-                        pageController.page?.round() ?? 0;
-                    const int lastPageIndex = 2;
-                    if (currentPageIndex == lastPageIndex) {
-                      // Navigate to the home page
-                      viewModel.goToLoginPage();
-                    } else {
-                      if (currentPageIndex == 1) {
-                        viewModel.changeText();
-                      }
-                      // Move to the next page
-                      pageController.nextPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut,
-                      );
-                    }
-                  },
-                )),
+            Expanded(
+              child: PageView(
+                controller: viewModel.pageController,
+                physics: const AlwaysScrollableScrollPhysics(),
+                onPageChanged: viewModel.onPageChanged,
+                children: const [
+                  PageViewContent(
+                    image: PngImages.coolKidsLongDistanceRelationship1,
+                    title: AppConstants.intro1TitleText,
+                  ),
+                  PageViewContent(
+                    image: PngImages.coolKidsStayingHome,
+                    title: AppConstants.intro2TitleText,
+                  ),
+                  PageViewContent(
+                    image: PngImages.coolKidsHighTech1,
+                    title: AppConstants.intro3TitleText,
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.topCenter,
+              height: 6,
+              width: 52,
+              margin: const EdgeInsets.only(top: 16, bottom: 70),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  PageViewBar(
+                    index: 0,
+                    currentIndex: viewModel.currentPageIndex,
+                  ),
+                  PageViewBar(
+                    index: 1,
+                    currentIndex: viewModel.currentPageIndex,
+                  ),
+                  PageViewBar(
+                    index: 2,
+                    currentIndex: viewModel.currentPageIndex,
+                  ),
+                ],
+              ),
+            ),
+            AppButton(
+              title: viewModel.buttonText,
+              onClick: viewModel.buttonClick,
+            ),
           ],
         ),
       ),
@@ -300,4 +97,76 @@ class IntroPage extends StackedView<IntroViewModel> {
     BuildContext context,
   ) =>
       IntroViewModel();
+}
+
+class PageViewContent extends StatelessWidget {
+  const PageViewContent({super.key, required this.image, required this.title});
+
+  final String image;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(image),
+        const SizedBox(height: 16),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Color(0xFF3B3936),
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+            height: 1.33,
+            letterSpacing: -0.50,
+          ),
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          AppConstants.introDescriptionText,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color(0xFF78746D),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            height: 1.5,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PageViewBar extends StatelessWidget {
+  const PageViewBar(
+      {super.key, required this.index, required this.currentIndex});
+
+  final int index;
+  final int currentIndex;
+  final _isCurrentIndex = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: checkCurrentIndex() ? 16 : 6,
+      height: 6,
+      decoration: BoxDecoration(
+        color: checkCurrentIndex()
+          ? const Color(0xFF65A9E9)
+          : const Color(0xFFD5D4D4),
+      borderRadius: BorderRadius.circular(50),
+      ),
+    );
+  }
+
+  bool checkCurrentIndex() {
+    if (currentIndex == index) {
+      return true;
+    }
+    return false;
+  }
 }
